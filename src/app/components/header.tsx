8c4efd5f29aa079cfd0492a62f42bc7cf983850b
root <root@DESKTOP-00JC3MM.localdomain>
2026-03-07 04:41:17 +0200

    test commit to test github actions deployment

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -62,7 +62,7 @@ export default function Header() {
 
 				<div className='flex max-lg:ml-auto space-x-4'>
 					<button className='px-4 py-2 text-sm rounded-full font-medium cursor-pointer tracking-wide text-slate-900 border border-gray-400 bg-transparent hover:bg-gray-50 transition-all'>
-						Login with steam
+						Button
 					</button>
 
 					<button
